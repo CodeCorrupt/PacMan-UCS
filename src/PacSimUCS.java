f201c164865ebcdb8d0eac2cabd700c2c6fc65ad
Tyler Hoyt <tylerhoyt93@gmail.com>
2015-10-02 15:35:08 -0400

    Compleatly rewrote PacSimUCS, not finished

--- src/PacSimUCS.java
+++ src/PacSimUCS.java
@@ -3,15 +3,8 @@ import pacsim.*;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
-/**
- *
- * @author glinosd
- */
 public class PacSimUCS implements PacAction {
 
     private Queue<PacFace> moves;
@@ -22,6 +15,8 @@ public class PacSimUCS implements PacAction {
     }
 
     public static void main( String[] args ) {
+
+        //new PacSimReplan( args[ 0 ] );
 
         String fname ="";
         if( args.length > 0 ) {
@@ -47,10 +42,9 @@ public class PacSimUCS implements PacAction {
 
     @Override
     public PacFace action( Object state ) {
-
+        //Store the grid passed to in
         PacCell[][] grid = (PacCell[][]) state;
-        PacmanCell pc = PacUtils.findPacman( grid );
-        PacFace newFace = null;
+        PacmanCell pc = PacUtils.findPacman(grid);
 
         // make sure Pacman is in this game
         if (pc == null) {
@@ -65,146 +59,118 @@ public class PacSimUCS implements PacAction {
         }
 
         // If there are no moves in the moves queue then create some
-        if (moves.peek() == null) {
+        if (moves == null || moves.peek() == null) {
             moves = UniformCostUtils.generateMoves(grid);
-            printPath(moves);
+            // If moves is still null then there must have been an error
+            if (moves == null || moves.peek() == null) {
+                System.out.println("ERROR: Unable to generate moves");
+                return null;
+            }
         }
-
+        // Return the next move and dequeue it.
         return moves.poll();
-    }
-
-    public void printPath(Queue<PacFace> faces) {
-        System.out.println("I should probably make this print the moves");
     }
 }
 
 class UniformCostUtils {
     public static Queue<PacFace> generateMoves(PacCell[][] grid) {
-        //Get a list of all food locations
-        ArrayList<Target> foods = findFood(grid);
-
-        // Init the fringe
-        PriorityQueue<Fringe> fringe = new PriorityQueue<>();
-        ArrayList<Point> start_list = new ArrayList<Point>();
-        start_list.add(PacUtils.findPacman(grid).getLoc());
-        fringe.add(new Fringe(start_list, foods));
-
-        // Kick off UCS
-        Fringe answer = UCS(grid, fringe);
-
+        //Establish fringe
+        Fringe fringe = new Fringe(grid);
+        ArrayList<Food> foods = findFood(grid);
+        Queue<Direction> directions = UCS(grid, fringe);
         return null;
     }
 
-    private static ArrayList<Target> findFood(PacCell[][] grid) {
+    public static ArrayList<PacFace> pathTo(PacCell[][] grid, Point a, Point b, PacFace face) {
+        return null;
+    }
+
+    public static Queue<Direction> UCS(PacCell[][] grid, Fringe fringe) {
+        return null;
+    }
+
+    public static ArrayList<Food> findFood(PacCell[][] grid) {
         // Holder for list of points
-        ArrayList<Target> foods = new ArrayList<>();
+        ArrayList<Food> foods = new ArrayList<>();
         // Iterate over each cell, if it's food then add the location to the list
         for (PacCell[] row : grid) {
             for (PacCell cell : row) {
                 if (cell instanceof FoodCell)
-                    foods.add(new Target(cell.getLoc(), false)); //Add to the list
+                    foods.add(new Food(cell.getLoc(), false)); //Add to the list
             }
         }
         return foods;
     }
+}
 
-    private static Fringe UCS(PacCell[][] grid, PriorityQueue<Fringe> fringe) {
-        while (true) {
-            Fringe first = fringe.poll();
-            // Check for empty array
-            if (first == null) {
-                System.out.println("There was an error. Fringe is empty without goal");
-                return null;
-            }
-            if (first.goalTest()) //If the fringe compleats the goal then return the fringe
-                return first;
-            // For each unvisited food, add that to the fringe
-            for (Target trgt : first.getTargets()) {
-                if (!trgt.visited) {
-                    ArrayList<Point> newPath = new ArrayList<>(first.getOrder());
-                    newPath.addAll(pathTo(grid, first.getOrder().get(first.getOrder().size() - 1), trgt.loc));
-                    ArrayList<Target> newTargets = new ArrayList<>(first.getTargets());
-                    newTargets.set(first.getTargets().indexOf(trgt), new Target(trgt.loc, true));
-                    fringe.add(new Fringe(newPath, newTargets));
-                }
-            }
-        }
+// Stored the fringe and all methods to interact with it
+class Fringe {
+    private PriorityQueue<FringeElement> fringe;
+
+    public Fringe() {
+        fringe = new PriorityQueue<>();
     }
 
-    private static ArrayList<Point> pathTo(PacCell[][] grid, Point a, Point b) {
-        //Setup ArrayList to hold the path
-        ArrayList<Point> path = new ArrayList<>();
-        PacFace lastFace = PacFace.E; // Arbitrary first face
-
-        while (!(path.size() == 0 ? a : path.get(path.size() - 1)).equals(b)) {
-            PacFace nextFace = PacUtils.euclideanShortestToTarget(
-                    (path.size() == 0 ? a : path.get(path.size() - 1)) , lastFace, b, grid);
-            if (nextFace == null) {     //    ^v^v I've hard coded the East face in here because it does not matter.
-                nextFace = PacUtils.reverse(lastFace); // The code simply uses the face to determine if pacman has to revese
-            }
-            lastFace = nextFace;
-            path.add(ptAndFaceToPt((path.size() == 0 ? a : path.get(path.size() - 1)), nextFace)); //Add the new point to the list
-        }
-
-        return path;
+    public Fringe(PacCell[][] grid) {
+        fringe = new PriorityQueue<>();
+        //Create fringe element for pacman's start position
+        ArrayList<Direction> startDir = new ArrayList<>();
+        startDir.add(new Direction(PacUtils.findPacman(grid).getLoc(), PacUtils.findPacman(grid).getFace()));
+        ArrayList<Food> foods = UniformCostUtils.findFood(grid);
+        FringeElement start = new FringeElement(startDir, foods);
+        //Finally add new element to fringe
+        fringe.add(start);
     }
 
-    public static PacFace twoPtToFace(Point a, Point b) {
-        return null;
+    public FringeElement dequeueNext() {
+        return  fringe.poll();
     }
 
-    public static Point ptAndFaceToPt (Point start, PacFace face) {
-        Point retPt = new Point(start);
-        switch (face) {
-            case N: retPt.translate(  0, -1 ); break;
-            case E: retPt.translate(  1,  0 ); break;
-            case S: retPt.translate(  0,  1 ); break;
-            case W: retPt.translate( -1,  0 ); break;
-        }
-        return retPt;
+    public FringeElement lookNext() {
+        return fringe.peek();
+    }
+
+    public void add(FringeElement element) {
+        fringe.add(element);
+    }
+}
+
+// Class to store the information of fringe elements.
+//      Stores the path to that fringe element
+//      Stores the cost to that element by calculating the length
+class FringeElement implements Comparable<FringeElement> {
+    public ArrayList<Direction> directions;
+    public int cost;
+    public ArrayList<Food> foods;
+
+    public FringeElement (ArrayList<Direction> directions, ArrayList<Food> foods) {
+        this.directions = directions;
+        this.cost = directions.size();
+        this.foods = foods;
+    }
+
+    @Override
+    public int compareTo(FringeElement other) {
+        return Integer.valueOf(cost).compareTo(other.cost);
+    }
+}
+
+class Direction {
+    public Point pt;
+    public PacFace face;
+
+    public Direction (Point pt, PacFace face) {
+        this.pt = pt;
+        this.face = face;
     }
 }
 
 // Stores the target sell (food) and weather it has been visited (eaten) yet
-class Target {
+class Food {
     public Point loc;
     public boolean visited;
-    public Target(Point loc, boolean visited) {
+    public Food(Point loc, boolean visited) {
         this.loc = loc;
         this.visited = visited;
-    }
-}
-
-// Used as a priority queue, this will store the list of target points and the cost through them
-class Fringe implements Comparable<Fringe> {
-    private final ArrayList<Point> order;
-    private final int cost;
-    private final ArrayList<Target> targets;
-
-    public Fringe(ArrayList<Point> order, ArrayList<Target> targets) {
-        this.order = order;
-        this.cost = order.size();
-        this.targets = targets;
-    }
-
-    public ArrayList<Point> getOrder() {
-        return order;
-    }
-
-    public ArrayList<Target> getTargets() {
-        return targets;
-    }
-
-    public boolean goalTest() {
-        for (Target tgt : targets) {
-            if (tgt.visited == false)
-                return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int compareTo(Fringe other) {
-        return Integer.valueOf(cost).compareTo(other.cost);
     }
 }
